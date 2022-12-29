644df1dbc5e71356d949d905ee46b9220bb43d20
Dedogames <geoldery@gmail.com>
2022-12-29 10:58:39 -0300

    features:
    1 -Time Stamp

--- project/js/main.js
+++ project/js/main.js
@@ -1,15 +1,40 @@
 window.addEventListener('load',function(){//Incializa apenas após carregar toda a pagina
 
     
-    function main(){
-        //Inicializando o canvas onde será renderizado(mostrado) o game
-        const canvas = document.getElementById('main_canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.width = 500;
-        canvas.height = 500;
 
+    let lasttime = 0;
+    var game = null;
+    const canvas = document.getElementById('main_canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    var posX = 10;
+    var posY = 10;
+    function update(deltaTime){
+        posX += 1;
+        posY += 1;
+    }
+
+    function draw(context){
         ctx.fillStyle = 'white';
-        ctx.fillRect(10, 10, 100, 100);
+        ctx.fillRect(posX, posY, 100, 100);
+    }
+
+    function loopGame(timeStamp){
+        const deltaTime = timeStamp - lasttime;
+        lasttime = timeStamp;  
+
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        update(deltaTime);
+        draw(ctx);
+ 
+        requestAnimationFrame(loopGame);
+    }
+ 
+
+    function main(){ 
+        loopGame(0);
     }
 
     main();
